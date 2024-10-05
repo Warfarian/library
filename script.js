e1da5fcac83792711b3c201ed64ecef94ea61304
Mr_Robot <66792944+Warfarian@users.noreply.github.com>
2024-10-05 20:24:58 +0530

    basic functions working

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
-const myLibrary = [];
+const myLibrary = [
+    new Book("Lord of the rings", "J.R.R Tolkien",  "122 pages"),
+    new Book("Naked Lunch", "J.A.Bayona", "164 pages"),
+    new Book("A Song of Ice and Fire", "George R.R Martin", "435 pages")
+];
 
-// form functionality 
-
-let newBook = document.getElementById("addNew");
-let submit = document.getElementById("submit");
-
-document.getElementById("newBook").style.display = 'none';
-
-document.getElementById("newBook").style.display = 'none';
-
-submit.addEventListener('click', () =>{
-    document.getElementById("newBook").style.display = 'none';
-});
-
-newBook.addEventListener('click', (x)=>{
-    if (x == 1){
-        document.getElementById("newBook").style.display = 'none';
-    }
-    else{
-        document.getElementById("newBook").style.display = "block";
-    }
-});
-
-// functions 
-
-// constructor
-function Book(name,author,length,status){
+function Book(name,author,length){
     this.name = name;
     this.author = author;
     this.length = length;
-    this.status = status;
 }
 
-function addBooks(){
-    let addedBook = new Book();
-    console.log(addedBook);
-    
+let submit = document.querySelector('#submit');
+submit.addEventListener('click',function addBookToLibrary(event){
+    event.preventDefault();
+    let name = document.getElementById('name').value;
+    let author = document.getElementById('author').value;
+    let length = document.getElementById('length').value;
+    let addedBook = new Book(name,author,length);
+    myLibrary.push(addedBook);
+    appendBookToDOM(addedBook);
+});
+
+const display = document.querySelector('.display');
+
+function displayLibrary(){
+    for (let i =0; i<myLibrary.length; i++){
+        appendBookToDOM(myLibrary[i]);
+    }
 }
+
+function appendBookToDOM(book){
+    const cards = document.createElement('div');
+    cards.innerHTML = `Name: ${myLibrary[i].name} , Author: ${myLibrary[i].author} , Length: ${myLibrary[i].length}`;
+    display.appendChild(cards);
+}
+
+window.onload = displayLibrary;
